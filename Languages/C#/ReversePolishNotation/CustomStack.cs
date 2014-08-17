8c1ba9d1d85c1a3cdd595bc018372b6b59e08006
hobelinm <hobelinm@gmail.com>
2014-08-17 15:56:36 -0700

    Reverse Polish Notation Project

---
+++ Languages/C#/ReversePolishNotation/CustomStack.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversePolishNotation
+{
+    /// <summary>
+    /// Node Class used by the stack structure
+    /// </summary>
+    class Node
+    {
+        public double value { get; set; }
+        public Node prev { get; set; }
+
+        /// <summary>
+        /// Default Constructor
+        /// </summary>
+        public Node()
+        {
+            this.value = 0.0;
+        }
+
+        /// <summary>
+        /// Constructor with a given value
+        /// </summary>
+        /// <param name="value"></param>
+        public Node(double value)
+        {
+            this.value = value;
+        }
+
+        /// <summary>
+        /// Full constructor that specifies values for all the members
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="prev"></param>
+        public Node(double value, Node prev)
+        {
+            this.value = value;
+            this.prev = prev;
+        }
+    }
+
+    /// <summary>
+    /// Creates a stack with two elements space
+    /// </summary>
+    class CustomStack
+    {
+        public int count { get; set; }
+        private Node head = null;
+        private Node top = null;
+
+        public CustomStack()
+        {
+            this.count = 0;
+        }
+
+        // push, pop, peek, count
+        public void push(double value)
+        {
+            if (head == null)
+            {
+                head = new Node(value, null);
+                top = head;
+                this.count++;
+            }
+            else
+            {
+                Node item = new Node(value, top);
+                top = item;
+                this.count++;
+            }
+        }
+
+        public double pop()
+        {
+            if(top != null)
+            {
+                double topValue = top.value;
+                top = top.prev;
+                count--;
+                return topValue;
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+
+        public double peek()
+        {
+            if(top != null)
+            {
+                return top.value;
+            }
+            else
+            {
+                return 0.0;
+            }
+        }
+    }
+}
---
+++ Languages/C#/ReversePolishNotation/ReversePolishNotation.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversePolishNotation
+{
+    class ReversePolishNotation
+    {
+        /// <summary>
+        /// Reverse Polish Notation
+        /// Usage: ReversePolishNotation.exe '1 2 + 3 *'
+        /// would execute: (1 + 2) * 3
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            if(args.Length != 1)
+            {
+                Console.WriteLine("[ReversePolishNotation] Usage:");
+                Console.WriteLine("ReversePolishNotation.exe '1 2 + 3 *'");
+                return;
+            }
+
+            String[] operationTokens = args[0].Split(' ');
+            double result = 0.0;
+            CustomStack storage = new CustomStack();
+            for(int index = 0; index < operationTokens.Length; index++)
+            {
+                switch(operationTokens[index])
+                {
+                    case "+":
+                        // Addition
+                        if (storage.count > 1)
+                        {
+                            result = storage.pop() + storage.pop();
+                            storage.push(result);
+                        }
+                        else
+                        {
+                            Console.WriteLine("[ReversePolishNotation] Error: String '{0}' contains errors - ErrorCode: NotEnoughStackItems", args[0]);
+                            return;
+                        }
+                        break;
+                    case "-":
+                        // Substraction
+                        if (storage.count > 1)
+                        {
+                            double temp = storage.pop();
+                            result = storage.pop() - temp;
+                            storage.push(result);
+                        }
+                        else
+                        {
+                            Console.WriteLine("[ReversePolishNotation] Error: String '{0}' contains errors - ErrorCode: NotEnoughStackItems", args[0]);
+                            return;
+                        }
+                        break;
+                    case "*":
+                        // Multiplication
+                        if (storage.count > 1)
+                        {
+                            result = storage.pop() * storage.pop();
+                            storage.push(result);
+                        }
+                        else
+                        {
+                            Console.WriteLine("[ReversePolishNotation] Error: String '{0}' contains errors - ErrorCode: NotEnoughStackItems", args[0]);
+                            return;
+                        }
+                        break;
+                    case "/":
+                        // Division
+                        if(storage.count > 1)
+                        {
+                            double temp = storage.pop();
+                            result = storage.pop() / temp;
+                            storage.push(result);
+                        }
+                        else
+                        {
+                            Console.WriteLine("[ReversePolishNotation] Error: String '{0}' contains errors - ErrorCode: NotEnoughStackItems", args[0]);
+                            return;
+                        }
+                        break;
+                    default:
+                        // It's a number
+                        double value;
+                        try
+                        {
+                            value = Convert.ToDouble(operationTokens[index]); // An implementation for this can be a good exercise!
+                        }
+                        catch (FormatException)
+                        {
+                            Console.WriteLine("[ReversePolishNotation] Error: Unable to convert to {0} to Double", 
+                                operationTokens[index]);
+                            return;
+                        }
+
+                        storage.push(value);
+                        break;
+                }
+            }
+
+            Console.WriteLine("[ReversePolishNotation] Operation Completed with {0} remaining items on the stack", storage.count);
+            Console.WriteLine(storage.pop());
+        }
+    }
+}
